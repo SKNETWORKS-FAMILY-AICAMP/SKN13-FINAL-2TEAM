97d1dcb296a5ade0cf14bb87c31c555fdf9d48ee
subin0821 <subiny0821@gmail.com>
2025-07-30 14:28:46 +0900

    fix: [박수빈] social login add
    카카오, 구글 소셜로그인 기능 추가.

--- FAST_API_BASETOOL/main.py
+++ FAST_API_BASETOOL/main.py
@@ -10,8 +10,17 @@ from apps.cart.router import router as cart_router
 from apps.products.router import router as products_router
 from apps.survey.router import router as survey_router
 from apps.faq.router import router as faq_router
+from dotenv import load_dotenv
+from starlette.middleware.sessions import SessionMiddleware
+import os
+from apps.auth.oauth.google_oauth import router as google_oauth_router  # Google OAuth 라우터
+from apps.auth.oauth.kakao_oauth import router as kakao_oauth_router # Kakao OAuth 라우터
 
 app = FastAPI()
+
+load_dotenv()
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
+
 
 # Static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -23,6 +32,8 @@ app.include_router(home_router, tags=["Home"])
 # For other routers, the prefix is added. 
 # e.g., the /login path in auth/router.py becomes /auth/login
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(google_oauth_router, prefix="/auth", tags=["Google"])
+app.include_router(kakao_oauth_router, prefix="/auth", tags=["Kakao"])
 app.include_router(recommend_router, prefix="/recommend", tags=["Recommendation"])
 app.include_router(review_router, prefix="/review", tags=["Review"])
 app.include_router(cart_router, prefix="/cart", tags=["Cart"])
